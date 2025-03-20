da78b1c379b0897686ab89228a362268bffcdb88
abhish3k-397 <152806402+abhish3k-397@users.noreply.github.com>
2025-03-20 20:48:27 +0530

    Gui update (#2)
    * deleted unnecessary folder and fixed Graph in theme also added alert logging
    
    * alert logging and theme fix

--- tracker/logger.py
+++ tracker/logger.py
@@ -5,20 +5,33 @@ from tracker.monitor import get_top_processes
 
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
-log_file = os.path.join(log_dir, "memory_log.txt")
+
+# Memory usage log
+memory_log_file = os.path.join(log_dir, "memory_log.txt")
+memory_handler = RotatingFileHandler(memory_log_file, maxBytes=5 * 1024 * 1024, backupCount=3)
+
+# Alert log
+alert_log_file = os.path.join(log_dir, "alert_log.txt")
+alert_handler = RotatingFileHandler(alert_log_file, maxBytes=2 * 1024 * 1024, backupCount=2)
 
 log_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-handler = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=3)  
-handler.setFormatter(log_formatter)
+memory_handler.setFormatter(log_formatter)
+alert_handler.setFormatter(log_formatter)
 
-logger = logging.getLogger("MemoryLogger")
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
+memory_logger = logging.getLogger("MemoryLogger")
+memory_logger.setLevel(logging.INFO)
+memory_logger.addHandler(memory_handler)
+
+alert_logger = logging.getLogger("AlertLogger")
+alert_logger.setLevel(logging.INFO)
+alert_logger.addHandler(alert_handler)
 
 def log_memory(usage_percent):
     top_processes = get_top_processes(3)
     process_details = "; ".join([f"{p['name']} (PID: {p['pid']}) - {p['memory_percent']:.2f}%" for p in top_processes])
     
-    logger.info(f"Memory Usage: {usage_percent}%; Top Processes: {process_details}")
+    memory_logger.info(f"Memory Usage: {usage_percent}%; Top Processes: {process_details}")
 
+def log_alert(usage_percent):
+    alert_logger.warning(f"ALERT: High Memory Usage at {usage_percent}%")
